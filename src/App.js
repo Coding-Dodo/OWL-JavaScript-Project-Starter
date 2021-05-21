659d4ca1bd57c24cbb79c1814a11ccd8d5aef7b1
PhilDL <philippe.lattention@hotmail.fr>
2021-05-21 08:49:23 +0400

    [IMP] App HTML structure

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { Component, tags } from "@odoo/owl";
 import { MyComponent } from "./components/MyComponent";
 
 const APP_TEMPLATE = tags.xml/*xml*/ `
-<div t-name="App" class="" t-on-click="update">
+<main t-name="App" class="" t-on-click="update">
   <MyComponent/>
-</div>
+</main>
 `;
 
 export class App extends Component {
