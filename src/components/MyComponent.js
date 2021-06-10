dc0ee08e95f926d630677e9e1a990733ad0cd5bf
PhilDL <philippe.lattention@hotmail.fr>
2021-06-10 10:35:10 +0400

    [IMP] Added tailwind.config.js examples

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,8 +1,8 @@
 import { Component, useState, tags } from "@odoo/owl";
 
 const MY_COMPONENT_TEMPLATE = tags.xml/*xml*/ `
-<h1 class="my-4 text-3xl md:text-5xl text-purple-800 font-bold leading-tight text-center md:text-left slide-in-bottom-h1" t-on-click.prevent="update">
-  Hello <t t-esc="state.text"/> Odoo App
+<h1 class="my-4 text-2xl md:text-4xl text-gray-700 font-bold leading-tight text-center md:text-left slide-in-bottom-h1" t-on-click.prevent="update">
+  Hello <t t-esc="state.text"/> Starter Template
 </h1>
 `;
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ["./src/*.js", "./src/**/*.js", "./public/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        turquoise: "#74d5de",
+        "reddish-brown": "#ed6e58",
+        "faded-green": "#deebec",
+      },
+    },
   },
   variants: {
     extend: {},
